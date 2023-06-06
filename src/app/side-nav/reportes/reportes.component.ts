8d4388fa2fcbb0316b4c8dc3900c904788759bea
Tyrone <jhinta7@gmail.com>
2023-06-05 21:16:27 -0500

    s

--- src/app/side-nav/reportes/reportes.component.ts
+++ src/app/side-nav/reportes/reportes.component.ts
@@ -1,24 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-
-interface Solicitud {
-  codigo: string;
-  fechaSolicitud: string;
-  numRespuestas: number;
-  descripcion: string;
-}
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
-  styleUrls: ['./reportes.component.css']
+  styleUrls: ['./reportes.component.scss']
 })
 export class ReportesComponent implements OnInit {
-  displayedColumns: string[] = ['codigo', 'fechaSolicitud', 'numRespuestas', 'descripcion'];
-  dataSource = new MatTableDataSource<Solicitud>();
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private http: HttpClient, private dialog: MatDialog) { }
