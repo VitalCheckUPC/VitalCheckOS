22072a145cd1d27e5889a7c7caac38cc162aa027
Tyrone <jhinta7@gmail.com>
2023-06-05 19:35:48 -0500

    a

--- src/app/side-nav/reportes/reportes.component.ts
+++ src/app/side-nav/reportes/reportes.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { AgregarSolicitudModalComponent } from './agregar-solicitud-modal/agregar-solicitud-modal.component';
+
+interface Solicitud {
+  codigo: string;
+  fechaSolicitud: string;
+  numRespuestas: number;
+  descripcion: string;
+}
 
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
-  styleUrls: ['./reportes.component.scss']
+  styleUrls: ['./reportes.component.css']
 })
 export class ReportesComponent implements OnInit {
+  displayedColumns: string[] = ['codigo', 'fechaSolicitud', 'numRespuestas', 'descripcion'];
+  dataSource = new MatTableDataSource<Solicitud>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor() { }
+  constructor(private http: HttpClient, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.fetchData();
   }
 
+  fetchData(): void {
+    this.http.get<Solicitud[]>('URL_DE_TU_API').subscribe(data => {
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  agregarSolicitud(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '1500px';
+    const dialogRef = this.dialog.open(AgregarSolicitudModalComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        // Lógica para agregar una solicitud de abastecimiento
+        console.log('Solicitud agregada:', result);
+        this.fetchData(); // Actualizar la tabla con los nuevos datos
+      }
+    });
+  }
 }
